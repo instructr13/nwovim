d2c23f9ab347abc631a3f6b5544b6e5a0721d0f1
instructr13 <mido@wycey.dev>
2023-11-23 21:31:49 +0900

    add folding capabilities for nvim-ufo

--- lua/lsp/capabilities.lua
+++ lua/lsp/capabilities.lua
@@ -7,7 +7,15 @@ function M.make_capabilities()
   return vim.tbl_deep_extend(
     "force",
     vim.lsp.protocol.make_client_capabilities(),
-    require("cmp_nvim_lsp").default_capabilities()
+    require("cmp_nvim_lsp").default_capabilities(),
+    {
+      textDocument = {
+        foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true,
+        },
+      },
+    }
   )
 end
 
